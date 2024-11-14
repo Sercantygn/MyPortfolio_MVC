2e57024ed06b1ee84c7980c16e5b9c76935ce0b5
Sercan TOYGUN <sercan.toygun41@gmail.com>
2024-11-14 22:31:41 +0300

    Login Kısmı tasarlandı

--- MyPortfolio_MVC/Controllers/DefaultController.cs
+++ MyPortfolio_MVC/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyPortfolio_MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,28 @@ namespace MyPortfolio_MVC.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+        MyPortfolioDbEntities db = new MyPortfolioDbEntities();
         public ActionResult Index()
         {
             return View();
         }
+        public PartialViewResult DefaultBanner()
+        {
+            var values = db.TblBanners.Where(x => x.IsShown == true).ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult DefaultExpertise()
+        {
+            var values = db.TblExpertises.ToList();
+
+            return PartialView(values);
+        }
+        public PartialViewResult DefaultExperience()
+        {
+            var values = db.TblExperiences.ToList();
+            return PartialView(values);
+        }
+
+
     }
 }
